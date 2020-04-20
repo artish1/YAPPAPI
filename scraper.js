d547dcc78d968a0586109770cbb639c6a3181ec3
Mark Artishuk <markyshuk@gmail.com>
2020-04-20 16:51:08 -0700

    made scraper function for getting pirate's crew

---
+++ scraper.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const ocean_name = "emerald";
+const pirate_name = "Hera";
+
+async function getPiratePage(pirate_name, ocean_name) {
+  const url = `http://${ocean_name}.puzzlepirates.com/yoweb/pirate.wm?target=${pirate_name}&classic=$classic`;
+  const response = await axios.get(url);
+
+  const $ = cheerio.load(response.data);
+  const font_elements = $("font");
+
+  const status = font_elements.first();
+
+  const crew = status.find("b a");
+  return crew.text();
+}
+const hello = getPiratePage(pirate_name, ocean_name);
+
+hello.then((res) => {
+  console.log("Crew: ", res);
+});
