1b76ff0cb73a7ce9f46f953a83764adc9c953da9
Mark Artishuk <markyshuk@gmail.com>
2020-04-20 18:19:38 -0700

    Removed unnecessary variables

--- scraper.js
+++ scraper.js
@@ -1,13 +1,9 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const ocean_name = "emerald";
-const pirate_name = "Hera";
-
 async function getPirateCrew(pirate_name, ocean_name) {
   const url = `http://${ocean_name}.puzzlepirates.com/yoweb/pirate.wm?target=${pirate_name}&classic=$classic`;
   const response = await axios.get(url);
-
   const $ = cheerio.load(response.data);
   const font_elements = $("font");
 
